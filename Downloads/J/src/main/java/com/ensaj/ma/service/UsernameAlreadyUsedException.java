db6428eac577afa3a07aeed1916bccfe3d37822d
Mahdia <Arakrak.mahdya@gmail.com>
2022-04-26 00:19:09 +0000

    Initial version of arrosage generated by generator-jhipster@7.7.0

---
+++ Downloads/J/src/main/java/com/ensaj/ma/service/UsernameAlreadyUsedException.java
@@ -0,0 +1,10 @@
+package com.ensaj.ma.service;
+
+public class UsernameAlreadyUsedException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public UsernameAlreadyUsedException() {
+        super("Login name already used!");
+    }
+}
